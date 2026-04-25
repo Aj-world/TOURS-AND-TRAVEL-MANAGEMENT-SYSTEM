4c97763e1d87b3ea4d9b3f9d7fdb7ded01ccf92c
Abinash Nayak <abinash.tech.ai@gmail.com>
2026-04-26 01:49:37 +0530

    feat: add Redis caching, Docker setup, and environment-based configuration (dev/prod profiles)

--- src/main/java/com/aj/travel/ToursAndTravelManagementSystemApplication.java
+++ src/main/java/com/aj/travel/ToursAndTravelManagementSystemApplication.java
@@ -2,8 +2,10 @@ package com.aj.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ToursAndTravelManagementSystemApplication {
 
     public static void main(String[] args) {
